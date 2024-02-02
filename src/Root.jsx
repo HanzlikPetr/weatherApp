7e660511cd3fabf3bde9151afa7a204c81548e21
PetrHanzlik <petaxhanzlik74@gmail.com>
2024-02-02 17:34:48 +0100

    reapir

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,5 +1,11 @@
+import React from "react"
 
 export default function Root(){
+    const favorites = Object.keys({...localStorage})
+    console.log(favorites)
+
+    
+
     return(
         <h1>Root</h1>
     )
